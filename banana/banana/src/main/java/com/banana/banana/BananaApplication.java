683a02ee264808a2f1f8ec9abad9ba5c41c8b4c5
galileo-dev <fionn2200@gmail.com>
2023-05-24 18:53:07 +0100

    workinggit add .

--- banana/banana/src/main/java/com/banana/banana/BananaApplication.java
+++ banana/banana/src/main/java/com/banana/banana/BananaApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = { "com.banana.banana.repository.ConsentFormRepository", "com.banana.banana" })
+@SpringBootApplication
 public class BananaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BananaApplication.class, args);
 	}
--- banana/banana/src/main/java/com/banana/banana/BananaController.java
+++ banana/banana/src/main/java/com/banana/banana/BananaController.java
@@ -3,22 +3,27 @@ package com.banana.banana;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // Importing required classes
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.banana.banana.models.ConsentForm;
-import com.banana.banana.repository.ConsentFormRepository;
 
 @Controller
-@RequestMapping
+// @EnableJpaRepositories(basePackages = "com.banana.banana.repository")
+@RequestMapping("/")
 public class BananaController {
 
-    @Autowired()
-    private ConsentFormRepository consentFormRepository;
+    private final BananaService bananaService;
+
+    public BananaController(BananaService bananaService) {
+        this.bananaService = bananaService;
+    }
 
     @GetMapping("/ice_cream")
     public String ice_cream() {
@@ -32,10 +37,9 @@ public class BananaController {
 
     @PostMapping("/consent/submit-form")
     public String consentSubmit(@ModelAttribute ConsentForm consentForm) {
-        // save consentForm to database
+
         consentForm.setDateSigned(LocalDate.now());
-        consentFormRepository.save(consentForm);
-        // consentForm.save(
+        bananaService.saveConsentForm(consentForm);
 
         return "success";
     }
